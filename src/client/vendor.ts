6242dd382ef7fa742474c3b844c459d3a675598b
Olivier MICHAUD <o.michaud@tpl-ct-debian8>
2017-12-22 16:46:58 +0100

    build

--- src/client/vendor.ts
+++ src/client/vendor.ts
@@ -10,14 +10,13 @@ import '!style-loader!css-loader!jqvmap/dist/jqvmap.min.css';
 import '!style-loader!css-loader!bootstrap-daterangepicker/daterangepicker.css';
 
 
-import '!style-loader!css-loader!../static/build/css/custom.min.css';
+import '!style-loader!css-loader!../../static/build/css/custom.min.css';
 //import '../static/build/js/custom.min.js';
 
 //import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'fastclick/lib/fastclick.js';
 import 'nprogress/nprogress.js';
-import 'Chart.js/dist/Chart.min.js';
 import 'gauge.js/dist/gauge.min.js';
 import 'bootstrap-progressbar/bootstrap-progressbar.min.js';
 import 'iCheck/icheck.min.js';
@@ -30,8 +29,10 @@ import 'Flot/jquery.flot.resize.js';
 import 'flot.orderbars/js/jquery.flot.orderBars.js';
 import 'flot-spline/js/jquery.flot.spline.min.js';
 import 'flot.curvedlines/curvedLines.js';
-import 'Datejs';
 import 'jqvmap/dist/jquery.vmap.js';
 import 'jqvmap/dist/maps/jquery.vmap.world.js';
 import 'moment/min/moment.min.js';
 import 'bootstrap-daterangepicker/daterangepicker.js';
+
+//import 'Chart.js/dist/Chart.min.js';
+//import 'Datejs';
